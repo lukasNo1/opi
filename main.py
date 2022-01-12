4c380042f0fb46590bad6076a8e7f19cbc3406f9
lukasNo1 <lukas@flawless.ch>
2022-01-12 20:34:14 +0100

    Catch all non handled exceptions on a high level and handle them according to 'botAlert' config setting

--- main.py
+++ main.py
@@ -3,35 +3,39 @@ import time
 from configuration import apiKey, apiRedirectUri, debugMarketOpen
 from api import Api
 import datetime
+import alert
 
 api = Api(apiKey, apiRedirectUri)
 
 defaultWaitTime = 3600
 
-while True:
-    api.connect()
+try:
+    while True:
+        api.connect()
 
-    execWindow = api.getOptionExecutionWindow()
+        execWindow = api.getOptionExecutionWindow()
 
-    if debugMarketOpen or execWindow['open']:
-        print('Market open, running the program now ...')
-        writeCcs(api)
+        if debugMarketOpen or execWindow['open']:
+            print('Market open, running the program now ...')
+            writeCcs(api)
 
-        time.sleep(defaultWaitTime)
-    else:
-        if execWindow['openDate']:
-            print('Waiting for last market hour ...')
-
-            delta = execWindow['openDate'] - execWindow['nowDate']
-
-            if delta > datetime.timedelta(0):
-                print('Window open in: %s. waiting ...' % delta)
-                time.sleep(delta.total_seconds())
-            else:
-                # we are past open date, but the market is not open
-                print('Market closed already. Rechecking in 1 hour ...')
-                time.sleep(defaultWaitTime)
-
-        else:
-            print('The market is closed today, rechecking in 1 hour ...')
             time.sleep(defaultWaitTime)
+        else:
+            if execWindow['openDate']:
+                print('Waiting for last market hour ...')
+
+                delta = execWindow['openDate'] - execWindow['nowDate']
+
+                if delta > datetime.timedelta(0):
+                    print('Window open in: %s. waiting ...' % delta)
+                    time.sleep(delta.total_seconds())
+                else:
+                    # we are past open date, but the market is not open
+                    print('Market closed already. Rechecking in 1 hour ...')
+                    time.sleep(defaultWaitTime)
+
+            else:
+                print('The market is closed today, rechecking in 1 hour ...')
+                time.sleep(defaultWaitTime)
+except Exception as e:
+    alert.botFailed(None, 'Uncaught exception: ' + str(e))
